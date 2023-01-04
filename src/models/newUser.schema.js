bfafb5fd219ca6938c5411d28731d9e8d883c8ad
Gabriel Cardozo <gabriel.scardozo7@gmail.com>
2023-01-03 21:06:11 -0300

    refact: arquivos com nomes semânticos, e separação das rotas em arquivos distintos

--- src/models/newUser.schema.js
+++ src/models/newUser.schema.js
@@ -4,7 +4,7 @@ const newUserSchema = Joi.object({
     username: Joi.string().required(),
     email:Joi.string().email({minDomainSegments:2, tlds:{allow:['com','net']}}).required(),
     password: Joi.string().required(),
-    pictureUrl: Joi.string().required()
+    pictureUrl: Joi.string().uri().required()
 });
 
 
