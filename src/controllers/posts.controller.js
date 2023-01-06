17b165644c4491eb4e2f7f2e0f697ca254ea797f
gusgusz <gustavoguimaraescamps@gmail.com>
2023-01-06 17:54:41 -0300

    get hashtags on postsControllers

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -11,28 +11,33 @@ export const getPosts = async (req, res) => {
   
 
   try{
+    const hashtags = (await connectionDb.query(`SELECT hashtags.name, COUNT("hashtagPosts"."hashtagId") FROM "hashtagPosts"
+    JOIN hashtags ON "hashtagPosts"."hashtagId" = hashtags.id 
+    group by hashtags.id
+    `)).rows;
+
   const response = await connectionDb.query(
     `SELECT users.username, users."pictureUrl", posts.* FROM posts JOIN users ON posts."userId" = users.id 
     ORDER BY posts."createdAt" DESC;`
     );
+    
     if(response.rowCount === 0) {
       return res.status(404).send("There are no posts yet");
     }
     const data = response.rows;
-    
+    console.log(data);
+  
+
     const posts = await Promise.all(data.map(async (post) => {
-      if(post.caption.includes("#")){
-        const hashtags = post.caption.match(/#[a-zA-Z]+/g);
-        post.hashtags = hashtags;
-      }
       const { url } = post;
       const metadata = await urlMetadata(url);
       const { title, description, image } = metadata;
       delete post.createdAt;
       return { ...post, title, description, image };
     }));
+    console.log(posts);
    
-  res.status(200).send(posts);
+  res.status(200).send({hashtags,posts});
   } catch (error) {
     console.log(error);
     res.status(500).send("An error occurred while trying to fetch the posts, please refresh the page");
