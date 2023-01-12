88a1af258d31fb013c80790aa2efc461e0fd64b0
Paulo-Junio <paulofernandesbraga@gmail.com>
2023-01-12 14:37:51 -0300

    FIX: get post query

--- src/repositories/getPosts.js
+++ src/repositories/getPosts.js
@@ -1,35 +1,40 @@
 import { connectionDb} from "../database/db.js";
 
-export const getPostsUser= async(res, userId, followStatus)=>{
+export const getPostsUser= async(res, userId, followStatus, page)=>{
     try{
         const statusFollow = followStatus;
         let response;
         
         if(statusFollow =="ok"){
-            
+            console.log("entrei 1")
             response = (await connectionDb.query(
                 `SELECT users.username, users."pictureUrl", posts.*, 
-                COALESCE(COUNT(likes."postId"),0) AS "numberOfLikes"
+                COALESCE(COUNT(likes."postId"),0) AS "numberOfLikes",
+                (select count (*) from likes where "postId"=posts.id and "userId"=$1)::int as liked
                  FROM posts 
                  LEFT JOIN users ON posts."userId" = users.id
                  LEFT JOIN likes ON likes."postId" = posts.id
                  JOIN follows ON follows."userId"=$1 WHERE (follows."userId"=$1 AND follows."followId"=users.id) OR users.id=$1
                  GROUP BY users.username, users."pictureUrl", posts.id 
-                 ORDER BY posts."createdAt" DESC LIMIT 10;`,[userId])).rows;
+                 ORDER BY posts."createdAt" DESC OFFSET $2 LIMIT 10;`,[userId,(page*10)])).rows;
+
         } else{
+            console.log("entrei 2")
             response = (await connectionDb.query(
                 `SELECT users.username, users."pictureUrl", posts.*, 
-                COALESCE(COUNT(likes."postId"),0) AS "numberOfLikes"
+                COALESCE(COUNT(likes."postId"),0) AS "numberOfLikes",
+                (select count (*) from likes where "postId"=posts.id and "userId"=$1)::int as liked
                  FROM posts 
                  LEFT JOIN users ON posts."userId" = users.id
                  LEFT JOIN likes ON likes."postId" = posts.id WHERE users.id=$1
                  GROUP BY users.username, users."pictureUrl", posts.id 
-                 ORDER BY posts."createdAt" DESC LIMIT 10;`,[userId])).rows;
+                 ORDER BY posts."createdAt" DESC  OFFSET $2 LIMIT 10;`,
+                 [userId, (page*10)])).rows;
         }
 
         return response;
     }catch(error){
         console.log(error)
-        res,sendStatus(500)
+        res.sendStatus(500)
     }
 }
